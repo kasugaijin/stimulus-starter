35a168487cad7c943e3f770437fb5759f6dc8f71
Ben <robinsonbena@gmail.com>
2022-06-25 11:00:50 -0700

    add slideshow markup and controller

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -3,8 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['source']
+  static classes = [ "supported" ]
   
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+
+  // Note: The clipboard-write permission name is not supported in Firefox, only Chromium browsers.
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard#using_the_clipboard_api
+  connect() {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state === "granted") {
+        this.element.classList.add(this.supportedClass);
+      }
+    });
   }
 }
