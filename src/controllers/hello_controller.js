63d3d802da868f098af23b388080297c62a765bf
Ben <robinsonbena@gmail.com>
2022-06-25 09:22:35 -0700

    Add data target to DOM element and set static target in controller

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -2,7 +2,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log('Hello, sexy!', this.element)
+  static targets = ['name']
+  
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
   }
 }
