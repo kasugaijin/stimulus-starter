6cdf980cbabce6c003f293c912f22fba635fb236
Ben <robinsonbena@gmail.com>
2022-06-25 09:38:01 -0700

    add clipboard copy markup and controller

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,10 @@
+// src/controllers/clipboard_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ['source']
+  
+  copy() {
+    navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+}
--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -5,8 +5,10 @@ export default class extends Controller {
   static targets = ['name']
   
   greet() {
-    const element = this.nameTarget
-    const name = element.value
-    console.log(`Hello, ${name}!`)
+    console.log(`Hello, ${this.name}!`)
+  }
+
+  get name() {
+    return this.nameTarget.value
   }
 }
